7e4bea8f3e78c383243a81605a81f52c933a7a1c
Mahbubullah <mahbubullahpathan@gmail.com>
2024-01-09 03:12:59 +0600

    add encryption

---
+++ src/Boob.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Boob = () => {
+    return (
+        <div>
+            <h2>Boob</h2>
+        </div>
+    );
+};
+
+export default Boob;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Alice from "./Alice";
+import Boob from "./Boob";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Alice />,
+    
+  },
+  {
+    path: "bob",
+    element: <Boob />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
